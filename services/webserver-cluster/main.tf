66abe6e3462127f9ee2670b3005014ae088ecb48
Po <davidyagoo@gmail.com>
2023-12-27 00:20:37 +0900

    Modified something

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -35,7 +35,7 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 }
 
-resource "aws_security_group_rule" "allow_http_inbound" {
+resource "aws_security_group_rule" "allow_http_server_port_inbound" {
   type              = "ingress"
   security_group_id = aws_security_group.instance.id
 
